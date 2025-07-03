1bb670600b88d10d165e5b641f21ef07a5ce5021
AmitRaj <alanamit91355@gmail.com>
2025-07-03 08:59:50 +0530

    Seperate my js from html

---
+++ app.js
@@ -0,0 +1,16 @@
+// main.js
+
+// Typed.js setup
+const typeData = new Typed(".role", {
+  strings: [
+    "Web Developer",
+    "Full Stack Developer",
+    "UI-UX Designer",
+    "Backend Developer",
+    "Coder",
+  ],
+  loop: true,
+  typeSpeed: 100,
+  backSpeed: 80,
+  backDelay: 1000,
+});
